feb34fd315aad83ad078b84f3872654f61ff4f10
Emmanuel VAISSE <emmanuel.vaisse@boursorama.fr>
2017-01-19 18:20:15 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# node-appevent
-A basic nodejs app container to build evented apps with env/argv based configuration
+# AppEvent
+
+A basic nodejs app container to build evented apps with env/argv based configuration.
+
+ - Almost a non-existant API.
+ - Basic wrapper around `process` global to setup an event-based application.
+ - PM2/PMX integration to abstract process usage & metrics 
+ - out-of-the box JSON logging with trace
+ - runtime commands using PM2 `trigger`
+ - system based configuration regarding `process.env` merged with `.env` file on the root folder, 
+and finally overrided by `yargs` command line arguments.
+
+# usage
+
+A basic usage 
+
+
+```javascript
+
+
+var start = require('appevent')();
+
+/*
+    Register module on boot
+ */
+process.once('boot', function MyModule() {
+    
+    /*
+        Do some registration works
+     */
+    process.args.FOO_BAR = {
+        description: 'Do something great',
+        default: "nope"
+    };
+
+    /* 
+        configuration/registration done
+        eventually inform other module or passing an API
+     */
+    process.on("ready", function () {
+        process.emit('boot:mymodule', {
+            myApi: () => {}, // custom api method;
+        });
+    });
+
+    process.once('start', function () {
+
+        /*
+            All configuration & dependencies merged,  
+            you can use config to setup your module
+         */
+        console.log(process.env.FOO_BAR);
+
+    });
+
+});
+
+process.on('boot', require('my-middleware').boot);
+
+
+/*
+    start app
+ */ 
+start();
+
+```
---
+++ appevent.js
@@ -0,0 +1,96 @@
+/**
+ * @author evaisse
+ */
+var restoreConsole = require('jsonconsole')();
+var dotenv = require('dotenv').config({ silent: true });
+var nconf = require('nconf');
+var os = require('os');
+var util = require('util');
+
+
+var defaultConfig = {
+
+    "NODE_ENV": {
+        alias: "env",
+        description: 'Global node env',
+        default: "production"
+    },
+
+    "OUTPUT_VERBOSE": {
+        alias: "verbose",
+        description: 'Enable debug level output',
+        default: false,
+        type: "boolean",
+    },
+
+    "h": {
+        description: "Show this help usage",
+        type: "boolean",
+    },
+
+    "V": {
+        description: "Show current version",
+        type: "boolean"
+    },
+
+};
+
+
+
+
+/*
+    Build environment regarding
+        1) process.env
+        2) dotenv() overrdie regarding .env file on root dir
+        3) argv values
+ */
+var buildEnvironmentConfig = function () {
+
+    /*
+        Assign env keys to config only assign 
+        config that match a CONSTANT_FORM_PATTERN LIKE_THAT
+     */
+    nconf.env(Object.keys(process.args))
+         .argv(process.args);
+
+    /*
+        Show help if needed
+     */
+    if (nconf.get('h')) {
+        process.stdout.write(nconf.stores.argv.help());
+        process.exit();
+    }
+
+    /*
+        Stop command for some args
+     */
+    if (nconf.get('V')) {
+        process.stdout.write(require(__dirname + '/package.json').version + os.EOL);
+        process.exit();
+    }
+
+    /*
+        rewrite only env keys
+     */
+    Object.keys(nconf.get())
+          .filter((k) => k.match(/[A-Z0-9][A-Z0-9_]+[A-Z0-9]/))
+          .forEach((key) => process.env[key] = nconf.get(key));
+
+}
+
+
+
+module.exports = function () {
+
+    process.args = process.args || defaultConfig;
+
+    process.once('ready', buildEnvironmentConfig);
+
+    return function () {
+        process.emit('boot');
+        process.emit('ready');
+        console.log('Start application with configuration ' + util.inspect(process.env));
+        process.emit('start');
+    };
+
+};
